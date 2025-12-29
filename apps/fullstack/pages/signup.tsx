f59ae5eb9bcea482eabdb4ec4c37048b5606d8b3
adamsocki <socki.adam@gmail.com>
2025-12-28 22:30:30 -0600

    Update signup page copy to reflect rebrand from Placemark to Planner

--- apps/fullstack/pages/signup.tsx
+++ apps/fullstack/pages/signup.tsx
@@ -9,8 +9,8 @@ const SignupPage: BlitzPage = () => {
     <div className="grid md:grid-cols-2">
       <div className="hidden md:block pr-10">
         <div className="pb-6 text-lg text-gray-700 dark:text-gray-200">
-          With Placemark you can edit, analyze, and share geospatial data with a
-          fraction of the time and cost of traditional GIS.
+          With Planner you can create, edit, and generate, a wide array of urban planning documents and analysis with a
+          fraction of the time and cost of traditional planning projects.
         </div>
         <ul className="list-disc pl-6 text-purple-700 dark:text-purple-300">
           <li>500 maps</li>
@@ -19,7 +19,8 @@ const SignupPage: BlitzPage = () => {
           <li>10+ data formats</li>
           <li>REST APIs</li>
           <li>GIS operations</li>
-          <li>Share maps with a link</li>
+          <li>Identify Public Engagement Needs</li>
+          <li>Generate plans with a click</li>
           <li>Powerful table view</li>
         </ul>
       </div>
